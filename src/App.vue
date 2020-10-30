b0c25cc9f8932fc8becb6a8fb6b076416ca853a2
hackertaco <imsa1224@naver.com>
2020-10-31 01:45:58 +0900

    feat 01: ⚡️add hero

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,39 @@
 <template>
-<h1>Hey VUe3 from CLI</h1>
-<p>dfe</p>
+<h1>Dc Heroes</h1>
+<ul>
+    <li v-for="(hero, index) in dcHeroes" :key="index">{{hero.name}}</li>
+</ul>
+<form @submit.prevent="dcHeroes.push({name: newHero}); newHero=''">
+    <input v-model.number=" newHero" placeholder="type your hero" />
+    <button type="submit">Add Hero</button>
+</form>
 </template>
 
 <script>
-export default {};
+export default {
+
+    data() {
+        return {
+            isActive: false,
+            newHero: "",
+            dcHeroes: [{
+                    name: 'ironman'
+                },
+                {
+                    name: 'catwoman'
+                },
+                {
+                    name: 'american'
+                },
+            ]
+        }
+    },
+    methods: {
+
+    }
+}
 </script>
+
+<style scoped>
+
+</style>
