b5b424cf3985a5f901d056c608cfdb30f996539a
hackertaco <imsa1224@naver.com>
2020-10-31 03:13:37 +0900

    feat 01:⚡️add nav bar

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+<nav class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-2"><a href="">Dc Heroes</a>
+</nav>
+</template>
+
+<script>
+export default {
+    setup() {
+
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
