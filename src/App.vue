c5fdc2a3a7792e236c3494f69d0c5f2d2f7fa61c
hackertaco <imsa1224@naver.com>
2020-10-31 03:14:07 +0900

    refactor 01: 🛠divided app.js into two (w/dcheroes

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,22 @@
 <template>
-<h1>Dc Heroes {{countHero}}</h1>
-
-<ul>
-    <li v-for="(hero, index) in dcHeroes" :key="index">{{hero.name}}
-        <button @click="removeHero(index)">x</button>
-    </li>
-</ul>
-<form @submit.prevent="addHero">
-    <input v-model.number=" newHero" placeholder="type your hero" />
-    <button type="submit">Add Hero</button>
-</form>
+<AppHeader />
+<div class="w-full flex">
+    <DcHeroes />
+</div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader';
+import DcHeroes from './components/DcHeroes'
 export default {
-
-    data() {
-        return {
-            newHero: "",
-            dcHeroes: [{
-                    name: 'ironman'
-                },
-                {
-                    name: 'catwoman'
-                },
-                {
-                    name: 'american'
-                },
-            ]
-        }
+    components: {
+        AppHeader,
+        DcHeroes
     },
-    methods: {
-        addHero() {
-            if (this.newHero !== "") {
-                this.dcHeroes.push({
-                    name: this.newHero
-                });
-                this.newHero = ''
-            }
-        },
-        removeHero(index) {
-            this.dcHeroes = this.dcHeroes.filter((hero, i) => i !== index)
-        }
-    },
-    computed: {
-        countHero() {
-            return this.dcHeroes.length
-        },
 
-    }
 }
 </script>
 
-<style scoped>
+<style >
 
 </style>
---
+++ src/components/DcHeroes.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="m-auto">
+    <h1 class="text-2xl text-center">Dc Heroes {{countHero}}</h1>
+
+    <ul>
+        <li class="flex justify-between" v-for="(hero, index) in dcHeroes" :key="hero.name">
+            <div>{{hero.name}}</div>
+            <button @click="removeHero(index)">x</button>
+        </li>
+    </ul>
+    <form class="mt-10" @submit.prevent="addHero">
+        <input class="border rounded" v-model.number=" newHero" placeholder="type your hero" />
+        <button class="border rounded bg-gradient-to-t from-red-700 to-pink-500 text-sm text-white" type="submit">Add Hero</button>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newHero: "",
+            dcHeroes: [{
+                    name: 'ironman'
+                },
+                {
+                    name: 'catwoman'
+                },
+                {
+                    name: 'american'
+                },
+            ]
+        }
+    },
+    methods: {
+        addHero() {
+            if (this.newHero !== "") {
+                this.dcHeroes.push({
+                    name: this.newHero
+                });
+                this.newHero = ''
+            }
+        },
+        removeHero(index) {
+            this.dcHeroes = this.dcHeroes.filter((hero, i) => i !== index)
+        }
+    },
+    computed: {
+        countHero() {
+            return this.dcHeroes.length
+        },
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
