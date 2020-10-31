be8c80ecbacc9428da9b1a001e958801aa1e6beb
hackertaco <imsa1224@naver.com>
2020-10-31 13:30:02 +0900

    feat 02: ⚡️add explanation of calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -10,6 +10,7 @@
     </section>
     <section class="flex flex-wrap text-center">
         <p class="w-1/6 h-10" style="width:14.28%" v-for="num in startDay()" :key="num"></p>
+        <!--startDay가 있는 이유: 1일의 요일 인덱스를 줘서 그걸 가지고 1일 위치를 조정하기 위하여, 숫자를 구했으니 그 자리만큼 비워두면 된다.-->
         <p :class="currentDateClass(num)" class="w-1/6 h-10" style="width:14.28%" v-for="num in daysInMonth()" :key="num">{{num}}</p>
     </section>
     <section class="flex justify-between my-4">
@@ -32,6 +33,7 @@ export default {
     methods: {
         daysInMonth() {
             return new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
+            //달의 전체 일수를 구한다.
         },
         startDay() {
             return new Date(this.currentYear, this.currentMonth).getDay()
@@ -55,8 +57,10 @@ export default {
             }
         },
         currentDateClass(num) {
+
             const calendarFullDate = new Date(this.currentYear, this.currentMonth, num).toDateString()
-            const currentFullDate = new Date().toDateString()
+            const currentFullDate = new Date().toDateString();
+            //toDateString을 쓰는 이유는, new date를 했을 때 수반되는 시간 때문이다.
             return currentFullDate === calendarFullDate ? 'text-yellow-600' : ''
         }
     },
@@ -65,7 +69,7 @@ export default {
             return new Date(this.currentYear, this.currentMonth).toLocaleString("default", {
                 month: 'long'
             })
-
+            // 숫자로 표현되는 달을 글자로 바꾸는
         }
     }
 }
