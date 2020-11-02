da8bbf65e47bcfe27d97459ee34435c9174ef722
hackertaco <imsa1224@naver.com>
2020-11-02 14:47:31 +0900

    feat 04: ⚡️v-model markdown

---
+++ src/pages/Markdown.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="flex w-full flex-wrap">
+    <h1 class="w-full text-center text-3xl">Markdown App</h1>
+    <section class="flex w-10/12 m-auto h-screen">
+        <article class="w-1/2 border">
+            <textarea v-model="text" class="w-full h-full"></textarea>
+        </article>
+        <article class="w-1/2 border bg-gray-100">{{text}}</article>
+    </section>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: ''
+        }
+    }
+}
+</script>
+
+<style>
+ 
+ </style>
