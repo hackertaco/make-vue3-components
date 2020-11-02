7cb89a83cce5d9a649c57e959b63840003570ce1
hackertaco <imsa1224@naver.com>
2020-11-02 14:23:57 +0900

    feat 03:⚡️ connect router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
 <AppHeader />
 <div class="w-full flex">
-    <!--<DcHeroes />-->
-    <Calendar />
+    <router-view></router-view>
 </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader';
-// import DcHeroes from './components/DcHeroes'
-import Calendar from './components/Calendar'
 export default {
     components: {
         AppHeader,
-        // DcHeroes,
-        Calendar
     },
 
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/style.css'
+import router from './router'
+const app = createApp(App)
+app.use(router)
 
-createApp(App).mount('#app')
+app.mount('#app')
