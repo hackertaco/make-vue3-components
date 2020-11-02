cfd7ef6953f134a65757fd793f421826d631381f
hackertaco <imsa1224@naver.com>
2020-11-02 14:24:21 +0900

    feat 03:⚡️home page made

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="m-auto">
+    <h1 class="text-center">Welcome to Vue3 Home</h1>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
