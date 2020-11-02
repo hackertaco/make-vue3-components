4fdd4e096874c1305838e48f5aaebc1d1b530636
hackertaco <imsa1224@naver.com>
2020-11-02 14:47:07 +0900

    feat 04: ⚡️route markdown app

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,27 @@
 <template>
 <nav class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-2">
-    <router-link class="mx-2" to="/dc-heroes">Dc Heroes</router-link>
-    <router-link class="mx-2" to="/calendar">Calendar</router-link>
+    <router-link v-for="item in list" :key="item.to" class="mx-2" :to="item.to">{{item.title}}</router-link>
 </nav>
 </template>
 
 <script>
 export default {
-    setup() {
-
-        return {}
+    data() {
+        return {
+            list: [{
+                    title: 'Dc Heroes',
+                    to: '/dc-heroes'
+                },
+                {
+                    title: 'Calendar',
+                    to: '/calendar'
+                },
+                {
+                    title: 'Markdown',
+                    to: '/markdown'
+                }
+            ]
+        }
     }
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import {createRouter, createWebHistory} from 'vue-router';
 import Home from './pages/Home'
 import DcHeroes from './pages/DcHeroes'
 import Calendar from './pages/Calendar'
+import Markdown from './pages/Markdown'
 
 const routes = [
     {path: '/', component: Home},
     {path: '/dc-heroes', component: DcHeroes},
     {path: '/Calendar', component: Calendar},
+    {path: '/Markdown', component: Markdown}, 
 
 ]
 const router = createRouter({
