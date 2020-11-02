b118aecb35fd6f98810363b9028844cba5f6e166
hackertaco <imsa1224@naver.com>
2020-11-03 00:15:43 +0900

    feat 04: ⚡️markdown complete with mixins

--- src/pages/Markdown.vue
+++ src/pages/Markdown.vue
@@ -3,7 +3,7 @@
     <h1 class="w-full text-center text-3xl">Markdown App</h1>
     <section class="flex w-10/12 m-auto h-screen">
         <article class="w-1/2 border">
-            <textarea v-model.lazy="text" class="w-full h-full"></textarea>
+            <textarea class="w-full h-full" :value="text" @input="update"></textarea>
         </article>
         <article class="w-1/2 border bg-gray-100" v-html="markedText"></article>
     </section>
@@ -12,8 +12,10 @@
 
 <script>
 import marked from 'marked'
-export default {
+import debounce from '../utilities/mixins/debounce'
 
+export default {
+    mixins: [debounce],
     data() {
         return {
             text: '',
@@ -24,6 +26,15 @@ export default {
         markedText() {
             return marked(this.text)
         }
+    },
+    methods: {
+        update(e) {
+            const task = () =>
+                (this.text = e.target.value)
+            this.debounce(task, 500)
+
+        },
+
     }
 }
 </script>
