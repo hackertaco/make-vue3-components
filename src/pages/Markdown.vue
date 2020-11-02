dde9aefb25772a5420eee4e077c07c9a0cca260d
hackertaco <imsa1224@naver.com>
2020-11-02 15:10:02 +0900

    feat 04: ⚡️ applicate to markdown app

--- src/pages/Markdown.vue
+++ src/pages/Markdown.vue
@@ -3,18 +3,26 @@
     <h1 class="w-full text-center text-3xl">Markdown App</h1>
     <section class="flex w-10/12 m-auto h-screen">
         <article class="w-1/2 border">
-            <textarea v-model="text" class="w-full h-full"></textarea>
+            <textarea v-model.lazy="text" class="w-full h-full"></textarea>
         </article>
-        <article class="w-1/2 border bg-gray-100">{{text}}</article>
+        <article class="w-1/2 border bg-gray-100" v-html="markedText"></article>
     </section>
 </div>
 </template>
 
 <script>
+import marked from 'marked'
 export default {
+
     data() {
         return {
-            text: ''
+            text: '',
+
+        }
+    },
+    computed: {
+        markedText() {
+            return marked(this.text)
         }
     }
 }
