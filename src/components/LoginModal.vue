0631a95d47e85ed1ee03420d1fd7c7d43c2189e2
hackertaco <imsa1224@naver.com>
2020-11-05 01:15:29 +0900

    feat 06: ⚡️ realize modal and close modal event

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -1,9 +1,25 @@
 <template>
-login
+<div v-if="isOpen">
+    <section @click="isOpen = false" class="z-20 h-screen w-screen bg-gray-500 fixed top-0 opacity-50"></section>
+    <div class="absolute inset-0 ">
+        <div class="flex h-full">
+            <div class="z-30 m-auto bg-white p-2 rounded">
+                <h1>
+                    Login
+                </h1>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            isOpen: true
+        }
+    }
 
 }
 </script>
