73050c185c27cb9555154381210d03584416e658
hackertaco <imsa1224@naver.com>
2020-11-06 00:54:27 +0900

    refactor 06: 🛠google login component

---
+++ src/components/Login/Google.vue
@@ -0,0 +1,27 @@
+<template>
+<section class="my-5 text-center">
+    <button class="text-center border rounded px-2" @click="loginWithGoogle">
+        Login With Google
+    </button>
+</section>
+</template>
+
+<script>
+import firebase from '../../utilities/firebase'
+export default {
+    methods: {
+        loginWithGoogle() {
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(() => {
+                this.$emit("close-login-from-google")
+            }).catch(function () {
+
+            });
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -1,5 +1,5 @@
 <template>
-<section @click="close()" class="z-20 h-screen w-screen bg-gray-500 fixed top-0 opacity-50"></section>
+<section @click="close" class="z-20 h-screen w-screen bg-gray-500 fixed top-0 opacity-50"></section>
 <div class="absolute inset-0 ">
     <div class="flex h-full">
         <div class="z-30 m-auto bg-white p-2 rounded shadow w-1/3">
@@ -7,11 +7,7 @@
                 <h1 class="text-xl text-center">
                     Login
                 </h1>
-                <section class="my-5 text-center">
-                    <button class="text-center border rounded px-2" @click="loginWithGoogle()">
-                        Login With Google
-                    </button>
-                </section>
+                <Google @close-login-from-google="close" />
                 <p class="my-3 text-center">Or</p>
                 <form class="p-2 my-2" @submit.prevent="submit">
                     <div class="my-4">
@@ -39,7 +35,11 @@
 
 <script>
 import firebase from '../utilities/firebase';
+import Google from './Login/Google'
 export default {
+    components: {
+        Google
+    },
     data() {
         return {
             email: '',
@@ -64,14 +64,7 @@ export default {
         close() {
             this.$emit('close-login')
         },
-        loginWithGoogle() {
-            var provider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithPopup(provider).then(() => {
-                this.close();
-            }).catch(function () {
 
-            });
-        }
     },
     mounted() {
         this.$refs.newIdRef.focus()
