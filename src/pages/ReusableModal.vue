61d641500b740b901a80a5f96f2655de52a25745
hackertaco <imsa1224@naver.com>
2020-11-17 00:46:52 +0900

    feat:⚡️modal with slot name

--- src/pages/ReusableModal.vue
+++ src/pages/ReusableModal.vue
@@ -3,11 +3,30 @@
     <div class="m-auto">
       <h1 class="text-2xl my-5">Reusable Modal</h1>
       <Modal v-if="isModalOpen" @close="isModalOpen = false">
-        <h1 class="text-red-500">Red Modal</h1>
+        <template #title>Modal1</template>
+        <template #body>
+          <p>this is body modal1</p>
+        </template>
       </Modal>
-      <Modal v-if="isBlueOpen" @close="isBlueOpen = false"> </Modal>
-      <button @click="isModalOpen = true">Red Modal</button>
-      <button @click="isBlueOpen = true">Blue Modal</button>
+      <Modal v-if="isBlueOpen" @close="isBlueOpen = false">
+        <template #title>Modal2</template>
+        <template #body>
+          <p>this is body modal2</p>
+        </template>
+      </Modal>
+
+      <button
+        class="border rounded px-2 mx-4 bg-blue-500 text-white"
+        @click="isModalOpen = true"
+      >
+        Red Modal
+      </button>
+      <button
+        class="border rounded px-2 mx-4 bg-blue-500 text-white"
+        @click="isBlueOpen = true"
+      >
+        Blue Modal
+      </button>
     </div>
   </section>
 </template>
